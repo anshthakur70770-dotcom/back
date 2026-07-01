28832b786d3a3cf52644ec997c8726c25db5d793
Ansh Thakur <anshthakur70770@gmail.com>
2026-07-01 13:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,20 +34,20 @@ class reate_class(BaseModel):
     class_name:str
 
 @app.post("/signup")
-def signu(detail:signup_detail):
+async def signu(detail:signup_detail):
     return signup(detail.d_name,detail.name,detail.password,detail.role)
 
 
 @app.post("/login")
-def Log(detail:Login_detail):
+async def Log(detail:Login_detail):
     return login(detail.name,detail.password)
 
 @app.post("/dashboard")
-def show_cl(detail:Show_classes):
+async def show_cl(detail:Show_classes):
     return all_classes(detail.id)
 
 @app.post("/create_class")
-def c_class(detail:reate_class):
+async def c_class(detail:reate_class):
     return create_class(detail.id,detail.class_name)
 
 
