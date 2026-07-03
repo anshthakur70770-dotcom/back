3bc2bc30808545ef937e614df36b11867655483d
Ansh Thakur <anshthakur70770@gmail.com>
2026-07-03 14:01:55 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,23 @@
 from login import signup ,login # import login and signup function from login.py
 from database import supabase  # import supabase
 from fastapi import HTTPException
-
-
+import io
+import uuid
+"""
+{
+# all functions
+[is_id_correct] # check is id correct 
+[all classes] # find all classes resistered with the t_id
+[create_class] # create a new class for specific id
+[all_assigments] # find all assigments resistered with the class
+[create_assigment] # create a new assigment with images
+}
+"""
 
 def is_id_correct(t_id):
     try:
-        result = supabase.table("users").select("id").eq("id",t_id).execute()
+        result = supabase.table("users").select("name").eq("id",t_id).execute()
         return True
-        
     except :
         return False
         
@@ -48,3 +57,47 @@ def create_class(t_id,class_name):
         }
     except Exception as e:
         raise HTTPException(status_code=400,detail=str(e))
+
+def all_assigments(t_id,class_name):
+    is_correc = is_id_correct(t_id)
+    if is_correc == False:
+        raise HTTPException(500,detail = "id is incorrect")
+    try:
+        result = supabase.table("Assigment").select("name","discription","photos_url").eq("teacher_id",t_id).eq("class_name",class_name).execute()
+        return result.data
+    except Exception as e:
+        raise HTTPException(400,detail=str(e))
+
+def turn_image_to_url(photos:list[bytes]):
+    photo_url = []
+    for photo in photos:
+        name = uuid.uuid4()
+        path = f"images/assigment/{name}.jpg"
+        supabase.storage.from_("assigments").upload(path,photo)
+        photo_url.append(supabase.storage.from_("assigments").get_public_url(path))
+    return photo_url
+        
+def create_assigment(t_id,class_name,name,discription=None,photos=None):
+    try :
+        is_i = is_id_correct(t_id)
+        if is_i == False:
+            raise HTTPException(500,detail="is is incorrect")
+        photos_url = None
+        result = {
+            "teacher_id":t_id,
+            "class_name":class_name,
+            "name":name    }
+        if photos :
+            photos_url = turn_image_to_url(photos)
+            result["photos_url"] = photos_url
+        if discription:
+            result["discription"]=discription
+        try:
+            supabase.table("Assigment").insert(result).execute()
+            return {"msg":"assigment added sussefully"}
+        except Exception as e:
+            raise HTTPException(400,detail= str(e))
+    except Exception as e:
+        raise HTTPException(400,detail=str(e))
+
+
